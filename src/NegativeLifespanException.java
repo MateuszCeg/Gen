bf8ef9c3fd3449a120d100d9f016f468dd807af7
MateuszCeg <mateuszceg@onet.pl>
2025-04-03 17:15:07 +0200

    Add files via upload

---
+++ src/NegativeLifespanException.java
@@ -0,0 +1,5 @@
+public class NegativeLifespanException extends Exception{
+    public NegativeLifespanException(Person a) {
+        super(a.getFirstName()+" "+a.getLastName()+" "+"\nBłąd daty.");
+    }
+}
