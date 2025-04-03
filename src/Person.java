88e9e890aed5e7d4ccb3ac9a5df3e85be530437a
MateuszCeg <mateuszceg@onet.pl>
2025-04-03 17:14:33 +0200

    Update Person.java

--- src/Person.java
+++ src/Person.java
@@ -1,16 +1,54 @@
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.time.DateTimeException;
 import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
 import java.util.*;
 
 public class Person implements Comparable<Person>{
-
     private String firstName, lastName;
-    private LocalDate birthDay;
+    private LocalDate birthDay, deathDay;
     private Set<Person> children = new HashSet<>();
 
     public String getFirstName() {
         return firstName;
     }
 
+    public static Person fromCsvLine(String str){
+        String[] arr = str.split(",");
+        String[] name = arr[0].split(" ");
+        DateTimeFormatter format = DateTimeFormatter.ofPattern("dd.MM.yyyy");
+        LocalDate birthD = LocalDate.parse(arr[1], format);
+        LocalDate deathD;
+        if(arr[2] == "") {
+            deathD = null;
+        }
+        else {
+            deathD = LocalDate.parse(arr[2], format);
+        }
+
+        return new Person(name[0], name[1], birthD, deathD);
+    }
+    public static List<Person> fromCsv(String filePath) throws IOException {
+        FileReader file = new FileReader(filePath);
+        BufferedReader fopen = new BufferedReader(file);
+        List<Person> personList = new ArrayList<>();
+        String line = fopen.readLine();
+        while(line!=null){
+            line = fopen.readLine();
+            if (line == null) break;
+            Person osob = Person.fromCsvLine(line);
+            try {
+                osob.checkLifeSpan();
+            } catch (NegativeLifespanException e) {
+                System.err.println(e);
+            }
+            personList.add(osob);
+        }
+        return personList;
+    }
     public void setFirstName(String firstName) {
         this.firstName = firstName;
     }
@@ -23,10 +61,11 @@ public class Person implements Comparable<Person>{
         this.lastName = lastName;
     }
 
-    public Person(String firstName, String lastName, LocalDate birthDay) {
+    public Person(String firstName, String lastName, LocalDate birthDay, LocalDate deathDay) {
         this.firstName = firstName;
         this.lastName = lastName;
         this.birthDay = birthDay;
+        this.deathDay = deathDay;
     }
     public boolean adopt(Person child){
         return children.add(child);
@@ -51,9 +90,19 @@ public class Person implements Comparable<Person>{
         return childs;
     }
 
+    void checkLifeSpan() throws NegativeLifespanException {
+        if ((this.birthDay.isAfter(this.deathDay)) || (this.deathDay==null)){
+            throw new NegativeLifespanException(this);
+        }
+    }
     @Override
     public String toString() {
-        return firstName + " " + lastName  + ", urodzony: " + birthDay;
+        if(deathDay == null) {
+            return firstName + " " + lastName + ", urodzony: " + birthDay;
+        }
+        else{
+            return firstName + " " + lastName + ", urodzony: " + birthDay + ", zgon:" + deathDay;
+        }
     }
 
     @Override
